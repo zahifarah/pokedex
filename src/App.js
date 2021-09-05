389056dcc6d75690868f86e2ef472b4c6cdb2820
Zahi Farah <zahi.farah@protonmail.com>
2021-09-05 13:08:34 +0300

    Define Pokedex component + map over defaultProps to display all of them.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
-import Pokecard from "./Pokecard";
+import Pokedex from "./Pokedex";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokecard id="4" name="Charmander" type="fire" base_experience="62" />
+        <Pokedex />
       </div>
     );
   }
--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
+import "./Pokecard.css";
 
 class Pokecard extends Component {
   render() {
-    const API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
+    const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
+
+    console.log(this.props);
 
     return (
       <div className="Pokecard">
         <h1>{this.props.name}</h1>
-        <img src={API} alt="" />
-        <h2>{this.props.type}</h2>
-        <h2>{this.props.base_experience}</h2>
+        <img src={POKE_API} alt="this.props.name" />
+        <div>
+          <h2>{this.props.type}</h2>
+        </div>
+        <div>
+          <h2>{this.props.base_experience}</h2>
+        </div>
       </div>
     );
   }
