6b08b4e2b7f210ac60d0d4c68827a0eeaec24155
Zahi Farah <zahi.farah@protonmail.com>
2021-09-06 11:21:30 +0300

    Use new API endpoint.

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
 
+// const OLD_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
+
+const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
+
+let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
+
 class Pokecard extends Component {
   render() {
-    const POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
-
-    console.log(this.props);
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
 
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={POKE_API} alt="this.props.name" />
+        <img src={imgSrc} alt="this.props.name" />
         <div className="Pokecard-data">{this.props.type}</div>
         <div className="Pokecard-data">{this.props.base_experience}</div>
       </div>
