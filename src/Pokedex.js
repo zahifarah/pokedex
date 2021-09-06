a5e9b6aba6e3e81e73e4108097992af6311081be
Zahi Farah <zahi.farah@protonmail.com>
2021-09-06 16:11:38 +0300

    Add title variable that returns conditional className based on isWinner boolean prop.

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -4,13 +4,22 @@ import "./Pokedex.css";
 
 class Pokedex extends Component {
   render() {
+    const { exp, isWinner, player, pokemon } = this.props;
+    let title;
+
+    if (isWinner) {
+      title = <h2 className="Pokedex-winner">Winner</h2>;
+    } else {
+      title = <h2 className="Pokedex-loser">Loser</h2>;
+    }
+
     return (
       <div className="Pokedex">
-        <h1>Pokedex!</h1>
-        <p>Total Experience: {this.props.exp}</p>
-        <p>{this.props.isWinner ? "WINNER!" : "LOSER!"}</p>
+        <h1 className="Pokedex-player">{player}</h1>
+        <h4 className="Pokedex-xp">Total Experience: {exp}</h4>
+        <div className="Pokedex-result">{title}</div>
         <div className="Pokedex-cards">
-          {this.props.pokemon.map((p) => {
+          {pokemon.map((p) => {
             return (
               <Pokecard
                 key={p.id}
