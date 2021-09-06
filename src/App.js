615c81d36caa68734052017abfffe8c7f8afe166
Zahi Farah <zahi.farah@protonmail.com>
2021-09-06 12:33:10 +0300

    Create two distinct Pokedexes from within Pokegame.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
+import Pokegame from "./Pokegame";
 import "./App.css";
-import Pokedex from "./Pokedex";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Pokedex />
+        <Pokegame />
       </div>
     );
   }
