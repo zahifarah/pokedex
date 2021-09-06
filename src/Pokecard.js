391d88d5b40be9d916e3ef0ee957e6bfa9f7b763
Zahi Farah <zahi.farah@protonmail.com>
2021-09-06 16:10:54 +0300

    Clean up code, deconstruct "this.props".

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
 
-// const OLD_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
+// const OLD_POKE_API = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.props.id}.png`;
 
 const POKE_API = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/`;
 
@@ -9,14 +9,17 @@ let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
+    const { id, name, type, base_experience } = this.props;
+    let imgSrc = `${POKE_API}${padToThree(id)}.png`;
 
     return (
       <div className="Pokecard">
-        <h1 className="Pokecard-title">{this.props.name}</h1>
-        <img src={imgSrc} alt="this.props.name" />
-        <div className="Pokecard-data">{this.props.type}</div>
-        <div className="Pokecard-data">{this.props.base_experience}</div>
+        <h2 className="Pokecard-title">{name}</h2>
+        <div className="Pokecard-image">
+          <img src={imgSrc} alt={name} />
+        </div>
+        <div className="Pokecard-data">{type}</div>
+        <div className="Pokecard-data">{base_experience}</div>
       </div>
     );
   }
